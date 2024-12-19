193b55d95091c15109d56b133d8a7d90c8ff86c5
SyeddHassan <syeddhassanali@gmail.com>
2024-12-19 12:01:19 -0800

    added legal tem page

--- user-interface/src/components/routes/legal-team/legal-team-card.tsx
+++ user-interface/src/components/routes/legal-team/legal-team-card.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+
+import { Card, CardContent } from "@/components/ui/card";
+
+import LegalTeamEmployee01 from "../../../../public/images/our-legal-team-employee-01-img.png";
 
 const LegalTeamCard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Card>
+      <CardContent className="py-0 pt-4 px-4 w-full h-full relative">
+        <Image
+          src={LegalTeamEmployee01}
+          alt="Our Legal Team Employee"
+          className="relative"
+        />
+        <div className="w-[80%] p-2 bg-white/70 backdrop-blur-md absolute bottom-6 left-1/2 -translate-x-[50%] rounded-xl flex-center flex-col">
+          <h1 className="text-heading-color font-inter font-bold leading-[26px]">
+            Orlando Diggs
+          </h1>
+          <p className="text-[14px] leading-[22px]">Co-Founder abd CEO</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default LegalTeamCard
+export default LegalTeamCard;
--- user-interface/src/components/routes/legal-team/legal-team-section.tsx
+++ user-interface/src/components/routes/legal-team/legal-team-section.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import LegalTeamCard from "./legal-team-card";
+
 const LegalTeamSection = () => {
   return (
-    <section className="layout-standard section-margin-standard grid grid-cols-4 gap-x-4 gap-y-8">
-        
+    <section className="layout-standard section-margin-standard grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-x-4 gap-y-8">
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
+      <LegalTeamCard />
     </section>
   );
 };
